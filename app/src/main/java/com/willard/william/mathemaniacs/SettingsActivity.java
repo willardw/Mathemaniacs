f16989103d847549f83e3c0e31e267a91a228660
willardw <william@willard.com>
2021-03-30 11:52:00 -0700

    Updated for new android and api versions

--- app/src/main/java/com/willard/william/mathemaniacs/SettingsActivity.java
+++ app/src/main/java/com/willard/william/mathemaniacs/SettingsActivity.java
@@ -3,7 +3,7 @@ package com.willard.william.mathemaniacs;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
